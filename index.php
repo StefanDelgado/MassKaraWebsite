430f137c2c059872b897e010939e61d1009f8fb9
StefanDelgado <99627352+StefanDelgado@users.noreply.github.com>
2023-10-19 23:03:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MassKara</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+</head>
+<body>
+
+    <header>
+        <a href="#" class="logo"><img src="logo.png" alt=""></a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#shop">FoodTrip</a></li>
+            <li><a href="#delivery">Delivery</a></li>
+            <li><a href="#tour">Tourist Spots</a></li>
+            <li><a href="#about">About Masskara</a></li>
+        </ul>
+    </header>
+
+    <!-- Main -->
+    <section class="home" id="home">
+        <div class="home-overlay"></div> <!-- Add overlay div -->
+        <div class="home-text">
+            <span>Welcome To</span>
+            <h1>MassKara Festival 2023</h1>
+            <h2>Balik Yuhom</h2>
+        </div>
+    </section>
+    
+    <section class="shop" id="shop">
+        <div class="heading">
+            <span>Buy Now!</span>
+            <h1>Buy Some Bacolod Delicacy</h1>
+        </div>
+        <div class="shop-container">
+            <div class="box">
+                <div class="box-img">
+                    <img src="Chicken Masskara.jpg" alt="Chicken MassKara">
+                </div>
+                <div class="stars">
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star-half' ></i>
+                </div>
+                <h2>Chicken Masskara</h2>
+                <br>
+                <a target="_blank" href="https://web.facebook.com/MasskaraChickenInasalOfficial/?_rdc=1&_rdr" class="btn">Order Now</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="BongBongs.jpg" alt="BongBongs">
+                </div>
+                <div class="stars">
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star-half' ></i>
+                </div>
+                <h2>BongBongs</h2>
+                <br>
+                <a target="_blank" href="https://bongbongspiaya.com.ph" class="btn">Order Now</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="Erons.jpg" alt="CansiHouse">
+                </div>
+                <div class="stars">
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star' ></i>
+                    <i class='bx bxs-star-half' ></i>
+                </div>
+                <h2>Eron's Cansi House</h2>
+                <br>
+                <a target="_blank" href="https://web.facebook.com/EronsCansiHouse/?_rdc=1&_rdr" class="btn">Order Now</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- FoodTrip -->
+    <section class="delivery" id="delivery">
+        <div class="heading">
+            <span>Bacolod's Delicacy!</span>
+            <h1>Learn more about Bacolod's Special Trademark Delicacy!</h1>
+        </div>
+        <div class="container">
+            <div class="delivery-img">
+                <img src="Napole.jpg" alt="">
+            </div>
+            <div class="delivery-text">
+                <h2>Bacolod's Famous Napoleones</h2>
+                <p>Leones Napoleones, a trusted name for authentic Bacolod pasalubong, has been serving delighted customers for over 25 years. 
+                    They take away the worries of finding the perfect pasalubong to take home for Bacolod visitors. 
+                    In 1999, they partnered with Pendy's, and their delicious napoleones quickly became a favorite among tourists visiting Bacolod. 
+                    Over the years, they've perfected their napoleones recipe, making them the top choice for authentic and delicious Bacolod pasalubong.</p>
+                <a target="_blank" href="https://leonesnapoleones.com" class="btn">Order Now</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Tourist -->
+    <section class="tour" id="tour">
+        <div class="heading">
+            <span>Places to Visit!</span>
+            <h1>Tourist Spots</h1>
+        </div>
+        <div class="container">
+            <div class="tour-text">
+                <h2>Venture Around to View our Place</h2>
+                <p>• <a target="_blank" href="https://maps.app.goo.gl/eu6CRshG14xUrGbv5">Mambukal Mountain Resort</a></p>
+                <p>• <a target="_blank" href="https://maps.app.goo.gl/o4qbvw9Dv3dcgNuV8">San Sebastian Cathedral</a></p>
+                <p>• <a target="_blank" href="https://maps.app.goo.gl/nt7sisE9vjjhqx8n6">Bacolod New Government Center</a></p>
+                <p>• <a target="_blank" href="https://maps.app.goo.gl/gagxtkxTXKM6cZvz8">Bacolod Public Plaza</a></p>
+                <p>• <a target="_blank" href="https://maps.app.goo.gl/ydRfD8haezU7aLKR7">The Famous Place "The Ruins"</a></p>
+                <a target="_blank" href="https://guidetothephilippines.ph/articles/what-to-experience/bacolod-tourist-spots" class="btn">Learn More</a>
+            </div>
+            <div class="tour-img">
+                <img src="Ruins.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <!-- About Us -->
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="Mask.jpg" alt="">
+        </div>
+        <div class="about-text">
+            <h2>51th Celebration of Masskara</h2>
+            <p>The Bacolod Masskara Festival is an annual celebration held every fourth Sunday of October in the city of smiles. 
+                The festival's name, "Masskara," means "many smiling faces," and its defining feature is the masks adorned with smiling faces. 
+                It was initiated in 1980 and continues to be celebrated today. 
+                he festival emerged during a time of crisis when the local sugar cane industry, 
+                a primary source of livelihood, faced difficulties due to the influx of high fructose corn syrup from the USA. 
+                Additionally, the year of its inception was marked by tragedy, as a maritime accident involving the MV Don Juan claimed 750 lives. 
+                In response to these challenges and to boost the citizens' spirits, City Mayor Jose Montalvo introduced the "Festival of Smiles," 
+                now widely recognized as the Masskara festival.</p>
+            <a target="_blank" href="https://www.bacolodmasskara.com/?fbclid=IwAR1rQnd1RYHXf1a_ESUXxQ8Y_d8ASt9uNXTgouoVXtrgvkhUjMzl4eB0hrw" class="btn">Learn More</a>
+        </div>
+    </section>
+
+    <!-- Contacts -->
+    <section class="contact" id="contact">
+        <div class="social">
+            <a href="https://www.facebook.com/MasskaraFestivalBalikYuhum/photos"><i class='bx bxl-facebook'></i></a>
+            <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwjB1ruEgfGBAxWsTWwGHV9YDDQQFnoECAwQAQ&url=https%3A%2F%2Ftwitter.com%2Fbacolodmasskara%3Flang%3Den&usg=AOvVaw1vvydbqwnjdZx0qxFarelx&opi=89978449"><i class='bx bxl-twitter'></i></a>
+            <a href="https://www.instagram.com/explore/locations/1017421870/bacolod-masskara-festival/"><i class='bx bxl-instagram'></i></a>
+            <a href="https://www.youtube.com/watch?v=WyHH4R5fsms"><i class='bx bxl-youtube'></i></a>
+        </div>
+        <div class="links">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Use</a>
+            <a href="#">Our Company</a>
+        </div>
+        <p>&#169; JAVAGODS All Rights Reserved.</p>
+    </section>
+
+    <script src="method.js"></script>
+</body>
+</html>
